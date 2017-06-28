00f15c686bc09f4d29c1bba4f21ec2553807444c
Minseok Lee <lagender@gmail.com>
2017-06-28 23:29:24 +0900

    Created MandalArt Project

---
+++ src/main/java/com/mandalart/rnm/MandalArtApplication.java
@@ -0,0 +1,12 @@
+package com.mandalart.rnm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandalArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandalArtApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mandalart/rnm/MandalArtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mandalart.rnm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MandalArtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
